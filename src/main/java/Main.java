563d1c400d9b3470818d3b6176bf4e31313dd12a
Nickmdp <nicgutierrez@gmail.com>
2023-03-29 14:58:57 -0300

    Refactorizacion metodo
    Se refactorizo leer archivo csv de partidos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,18 @@ public class Main {
         Equipo eq[]=new Equipo[4];
         Partidos par[]=new Partidos[2];
         Integer j=0,i=0;
+        LeerPartidos(archResultados, eq, par, j, i);
+
+
+        Pronosticos pron1=new Pronosticos(eq[0],'X',' ',' ',eq[1],par[0]);
+        Pronosticos pron2=new Pronosticos(eq[2],' ','X',' ',eq[2],par[1]);
+        Integer sumatotal=0;
+        sumatotal=pron1.puntos()+pron2.puntos();
+        System.out.println("El puntaje obtenido es de : "+sumatotal);
+      //  System.out.println();
+    }
+
+    private static void LeerPartidos(Path archResultados, Equipo[] eq, Partidos[] par, Integer j, Integer i) {
         try {
             List<String> lineasArch = Files.readAllLines(archResultados);
             boolean primero = true;
@@ -23,9 +35,9 @@ public class Main {
                         String[] split = linea.split(";");
 
                         eq[j]= new Equipo(split[1],split[2]);
-                        eq[j+1]=new Equipo(split[6],split[7]);
-                        par[i++]=new Partidos(eq[j],eq[j+1],Integer.valueOf(split[3]),Integer.valueOf(split[4]));
-                        j+=2;
+                        eq[j +1]=new Equipo(split[6],split[7]);
+                        par[i++]=new Partidos(eq[j], eq[j +1],Integer.valueOf(split[3]),Integer.valueOf(split[4]));
+                        j +=2;
                     }
                 }
             }
@@ -34,22 +46,5 @@ public class Main {
             System.out.println("Fallo la apertura del archivo");
             System.exit(1);
         }
-/*
-        Equipo eq1=new Equipo("Argentina","Seleccionado");
-        Equipo eq2=new Equipo("Arabia Saudita","Seleccioando");
-        Equipo eq3=new Equipo("Polonia","Seleccioando");
-        Equipo eq4=new Equipo("Mexico","Seleccioando");
-/*
-        Partidos par1=new Partidos(eq[0],eq[1],1,2);
-        Partidos par2=new Partidos(eq[2],eq[3],0,0);
-
- */
-
-        Pronosticos pron1=new Pronosticos(eq[0],'X',' ',' ',eq[1],par[0]);
-        Pronosticos pron2=new Pronosticos(eq[2],' ','X',' ',eq[2],par[1]);
-        Integer sumatotal=0;
-        sumatotal=pron1.puntos()+pron2.puntos();
-        System.out.println("El puntaje obtenido es de : "+sumatotal);
-      //  System.out.println();
     }
 }
